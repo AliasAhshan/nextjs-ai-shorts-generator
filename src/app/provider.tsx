6b9cbc00cf2d1dff0d86e63f75845f67c7c43b6b
AliasAhshan <ahshan_1988@hotmail.com>
2025-06-16 13:12:24 -0400

    Added user insertion to provider for the db and changed layout a little

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,16 +1,40 @@
-import React, {ReactNode} from 'react';
+"use client"
+
+import { Users } from '@/configs/schema';
+import { useUser } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
+import React, { ReactNode, useEffect } from 'react';
+import { db } from '@/configs/dbconfig';
 
 type ProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
+function Provider({ children }: ProviderProps) {
+  const { user } = useUser();
 
-function Provider({children}: ProviderProps) {
-    return (
-        <div>
-            {children}
-        </div>
-    )
+  useEffect(() => {
+    const checkAndInsertUser = async () => {
+      if (!user) return;
+
+      const email = user?.primaryEmailAddress?.emailAddress;
+      if (!email) return;
+
+      const result = await db.select().from(Users).where(eq(Users.email, email));
+
+      if (!result[0]) {
+        await db.insert(Users).values({
+          name: user.fullName ?? 'Unnamed User',
+          email,
+          imageUrl: user.imageUrl,
+        });
+      }
+    };
+
+    checkAndInsertUser();
+  }, [user]);
+
+  return <div>{children}</div>;
 }
 
-export default Provider
+export default Provider;
