ed184045ec8e0cd1cb18dd186c197651ca50ea80
AliasAhshan <ahshan_1988@hotmail.com>
2025-06-16 12:40:51 -0400

    Added signup and signin pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
    <div>  
       <h2>placeholder</h2>
       <button>placeholder</button>
+
+      <UserButton/>
    </div> 
   );
 }
